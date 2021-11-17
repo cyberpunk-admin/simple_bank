618655badbc9ed46aa662d4ae2d4c400dab2c646
liuxc <liuxc784637889@gmail.com>
2021-11-17 17:21:47 +0800

    add table users

--- api/transfer.go
+++ api/transfer.go
@@ -12,7 +12,7 @@ type TransferRequest struct {
 	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
 	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
 	Amount        int64  `json:"amount" binding:"required,gt=0"`
-	Currency      string `json:"currency" binding:"required,oneof=USD EUR RMB"`
+	Currency      string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createTransfer(ctx *gin.Context) {
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.7.4
-	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0
 	github.com/golang/mock v1.6.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lib/pq v1.10.4
